f6253c08bdafd93358bba926ed38ce6c77ff6cbe
산책 <48800572+thecar1771@users.noreply.github.com>
2023-06-05 04:01:01 +0000

    Update 37 files

--- src/game.js
+++ src/game.js
@@ -13,6 +13,40 @@ $input.addEventListener("input", (textData) => {
 });
 
 const onButtonClick = () => {
+  if (!newWord) {
+    alert("입력 후 진행");
+    return;
+  } else {
+    if (!word) {
+      word = newWord;
+      $word.textContent = word; // 제시어 추가
+    } else if (wordArr.slice(-1).toString().slice(-1) !== newWord.slice(0, 1)) {
+      alert("틀림");
+      return;
+    }
+
+    wordArr.push(newWord); // 입력값 저장
+    $input.value = ""; // 입력 field 초기화
+    $input.focus();
+
+    //참가자 순서를 변경
+    const order = parseInt($order.textContent);
+    if (order >= number) {
+      $order.textContent = 1;
+    } else {
+      $order.textContent = order + 1;
+    }
+    //입력 목록
+    $listP.textContent = wordArr.toString();
+    newWord = "";
+  }
+
+  /*
+  if (!newWord) {
+    alert("입력 후 진행");
+    return;
+  }
+
   if (!word) {
     //제시어가 비어 있을 경우
     word = newWord;
@@ -30,9 +64,9 @@ const onButtonClick = () => {
     }
     //입력 목록
     $listP.textContent = wordArr.toString();
+    newWord = "";
   } else {
     //제시어가 비어 있지 않을 경우
-
     //단어 끝 부분 비교
     if (wordArr.slice(-1).toString().slice(-1) === newWord.slice(0, 1)) {
       //단어 끝 부분이 같을 경우
@@ -49,11 +83,13 @@ const onButtonClick = () => {
       }
       //입력 목록
       $listP.textContent = wordArr.toString();
+      newWord = "";
     } else {
       //단어 끝 부분이 다를 경우
       alert("틀림");
     }
   }
+  */
 };
 document
   .querySelector("button[class='btn']")
