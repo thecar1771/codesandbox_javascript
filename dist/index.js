a2217fcf2193601ce8baa390a67a8dc13fa79402
산책 <48800572+thecar1771@users.noreply.github.com>
2023-06-21 03:59:16 +0000

    20230621

--- dist/index.js
+++ dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40887" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35625" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -338,4 +338,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/lotto.js.map
+//# sourceMappingURL=/index.js.map
